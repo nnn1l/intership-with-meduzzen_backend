4bad39c075a2ac23d3c6cf69be738e92f3951f65
Viktoria Verkholat <peremoga2009@gmail.com>
2026-06-17 20:00:14 +0300

    BE #13: Implemented a temporary storage solution using Redis to store quiz responses for 48 hours, following data validation and permissions guidelines.
    # Conflicts:
    #	app/services/quiz.py

--- app/services/quiz.py
+++ app/services/quiz.py
@@ -1,6 +1,9 @@
+import json
 from typing import TYPE_CHECKING, List
 
 from fastapi import HTTPException, status
+from redis.asyncio import Redis
+from sqlalchemy import select, and_, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..logger import logger
@@ -9,6 +12,8 @@ from ..models.quiz import Quiz
 from ..repositories.base import delete_from_db, get_with_pagination
 from ..repositories.quiz import create_quiz, get_quiz_by_id, update_quiz
 from ..schemas.quiz import QuizCreate, QuizUpdate
+from ..models.quiz import Quiz, Question, AnswerOption, QuizAttempt
+from ..schemas.quiz import QuizCreate, QuizUpdate, AnswerUpdate, QuizSubmit, UserAnswerSubmit
 
 if TYPE_CHECKING:
     from .company import CompanyService
@@ -85,5 +90,30 @@ class QuizService:
         logger.info(f"Quiz with ID {quiz_id} deleted successfully")
 
 
+
+
+    # TEMPORARILY SAVES ANSWER FOR 1 QUESTION IN REDIS FOR 48 HOURS
+    async def save_question_progress(self, redis: Redis, quiz_id: int, user_id: int, answer_data: UserAnswerSubmit):
+        redis_key = f"quiz_progress:{user_id}:{quiz_id}"
+
+        await redis.hset(redis_key, str(answer_data.question_id), json.dumps(answer_data.chosen_answer_id))
+        await redis.expire(redis_key, 172800) #48 hours
+
+
+    # GETS ALL STORED ANSWERS IN REDIS FOR PAST 48 HOURS
+    async def get_quiz_progress(self, redis: Redis, user_id: int, quiz_id: int) -> dict:
+        redis_key = f"quiz_progress:{user_id}:{quiz_id}"
+        stored_data = await redis.hgetall(redis_key)
+
+        if not stored_data:
+            return {}
+
+        return {int(q_id): json.loads(val) for q_id, val in stored_data.items()}
+
+
+    # DELETES CACHE IN REDIS AFTER SUCCESSFUL QUIZ SAVING IN POSTGRESQL
+    async def clear_quiz_progress(self, redis: Redis, user_id: int, quiz_id: int):
+        redis_key = f"quiz_progress:{user_id}:{quiz_id}"
+        await redis.delete(redis_key)
 
 
