65c3ae32e6ad69efa02ea01a0c1ef0128b9a1aef
Viktoria Verkholat <peremoga2009@gmail.com>
2026-06-22 22:15:21 +0300

    BE #16: Implemented a notification system according to data validation and permissions guidelines

--- app/services/notification.py
+++ app/services/notification.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import status, HTTPException
 
 from ..models.company import company_members
-from ..models.notification import Notification
+from ..models.notitcation import Notification
 from ..models.user import User
 from ..schemas.notification import NotificationCreate
 from ..logger import logger
