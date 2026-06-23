e7ca6d2f4c86fd0aae1677bdc63be8d5945f6b92
Viktoria Verkholat <peremoga2009@gmail.com>
2026-06-23 20:05:52 +0300

    fix migration, circular import, renamed file with incorrect word

--- app/models/notification.py
+++ app/models/notification.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 
 from sqlalchemy import ForeignKey, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from ..models import Base
+from .base import Base
 
 class Notification(Base):
     __tablename__ = 'notifications'
