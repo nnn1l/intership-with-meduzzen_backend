23e5ca1bc4f3834b6595afbe1243f7d7956983e9
Viktoria Verkholat <peremoga2009@gmail.com>
2026-06-23 18:40:57 +0300

    fix migration configuration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .company import Company, company_members
-from .quiz import Quiz, Question, AnswerOption, QuizAttempt
+from .quiz import Quiz, Question, AnswerOption
 from .user import User
 from .invitation import MembershipManagement
