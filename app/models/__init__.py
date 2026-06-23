92ca70baf19adbf49ad2aab6f8360a716ac76cab
Viktoria Verkholat <peremoga2009@gmail.com>
2026-06-23 20:05:52 +0300

    fix migration, circular import, renamed file with incorrect word

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .company import Company, company_members
 from .quiz import Quiz, Question, AnswerOption, QuizAttempt
 from .user import User
 from .invitation import MembershipManagement
+from .notification import Notification
